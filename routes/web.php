780b6c8af87c643b893ac92c664fc26e610bc6d6
MrZhang <zslemail@qq.com>
2026-08-14 14:41:06 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -48,12 +48,14 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
+// 用户个人中心路由
 Route::resource('users', 'UsersController', ['only'=> ['show', 'update', 'edit']]);
 
 
 
 
-
+// 话题路由
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 
 
