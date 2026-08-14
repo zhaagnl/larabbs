677de4cb5ba5fcb6b3698a1f14b16b926f9f8b6d
MrZhang <zslemail@qq.com>
2026-08-14 16:03:26 +0800

    填充用户和话题数据

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 生成数据集合
+        User::factory()->count(10)->create();
+
+        // 单独处理第一个用户数据
+        $user = User::find(1);
+        $user->name = 'MrZhang';
+        $user->email = 'zslemail@qq.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+
+
+    }
+}
