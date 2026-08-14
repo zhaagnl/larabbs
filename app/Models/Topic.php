da24a9cd24fb55788cf3ecc2eefed0bf1cb54941
MrZhang <zslemail@qq.com>
2026-08-14 20:29:30 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -7,6 +7,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Topic extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    // 一个话题属于一个分类，一对一关系
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // 一个话题属于一个作者
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
---
+++ resources/views/topics/_topic_list.blade.php
@@ -0,0 +1,53 @@
+@if(count($topics))
+  <ul class="list-unstyled">
+    @foreach($topics as $topic)
+      <li class="d-flex">
+        <div class=" ">
+          <a href="{{ route('users.show', [$topic->user_id]) }}">
+            <!-- 把 adventurer 换成 bottts、avataaars、croodles 等即可切换风格 -->
+            <img class="media-object img-thumbnail mr-3" style="width:52px;height:52px;"
+                @if($topic->user->avatar)
+                  src="https://api.dicebear.com/7.x/avataaars/svg?seed={{ $topic->user->name }}"
+                @else
+                  src="{{ $topic->user->avatar }}"
+                @endif title="{{ $topic->user->name }}">
+          </a>
+        </div>
+
+        <div class="flex-grow-1 ms-2">
+          <div class="mt-0 mb-1">
+            <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
+              {{ $topic->title }}
+            </a>
+            <a href="{{ route('topics.show',[$topic->id]) }}" class="float-end">
+              <span class="badge bg-secondary rounded-pill">{{ $topic->reply_count }}</span>
+            </a>
+          </div>
+
+          <small class="media-body meta text-secondary">
+              <a href="#" class="text-secondary" title="{{ $topic->category->name }}">
+                <i class="far fa-folder"></i>
+                {{ $topic->category->name }}
+              </a>
+
+              <span> • </span>
+              <a href="{{ route('users.show',[$topic->user_id]) }}" class="text-secondary">
+                <i class="far fa-user"></i>
+                {{ $topic->user->name }}
+              </a>
+              <span> • </span>
+              <i class="far fa-clock"></i>
+              <span class="timeage" title="最后活跃于：{{ $topic->updated_at }}">{{ $topic->updated_at->diffForHumans() }}</span>
+          </small>
+        </div>
+      </li>
+
+      @if(! $loop->last)
+        <hr>
+      @endif
+
+    @endforeach
+  </ul>
+@else
+  <div class="empty-block">暂无数据 ~_~ </div>
+@endif
