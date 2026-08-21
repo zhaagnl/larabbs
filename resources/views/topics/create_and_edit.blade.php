a72a85fb2d48661b95ca0c4fcc29fc49481f501f
MrZhang <zslemail@qq.com>
2026-08-21 15:44:11 +0800

    用户可以编辑话题

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -39,9 +39,11 @@
           <div class="mb-3">
             <label for="category_id-field">请选择分类</label>
             <select class="form-control" name="category_id" id="category_id-field" required>
-              <option value="" hidden disabled selected>请选择分类</option>
+              <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>请选择分类</option>
               @foreach($categories as $value)
-              <option value="{{ $value->id }}">{{ $value->name }}</option>
+              <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
+                {{ $value->name }}
+              </option>
               @endforeach
             </select>
           </div>
