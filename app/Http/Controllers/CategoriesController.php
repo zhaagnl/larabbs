5dce8daccec42fcdf0109481f13243aab6068364
MrZhang <zslemail@qq.com>
2026-09-06 17:12:11 +0800

    集成 laravel-permission 扩展包

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -11,7 +11,7 @@ class CategoriesController extends Controller
     public function show(Category $category, Request $request, Topic $topic)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
-        $topics = $topic->withOrder($request->order)
+        $topics = $topic->withOrder($request->order)//withOrder函数在模型中定义，传入排序参数
                         ->where('category_id', $category->id)
                         ->with('user', 'category') //预加载防止 N+1 问题
                         ->paginate(20);
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,12 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use App\Models\Topic;
 use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, MustVerifyEmailTrait;
+    use HasRoles;
 
     use Notifiable {
         notify as protected laravelNotify;
