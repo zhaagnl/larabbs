a953b557207071a0e59966ebf95c9204c088f0f3
MrZhang <zslemail@qq.com>
2026-09-08 12:37:07 +0800

    用户后台管理

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use App\Models\Topic;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -89,6 +90,29 @@ class User extends Authenticatable implements MustVerifyEmail
         $this->notification_count = 0;
         $this->save();
         $this->unreadNotifications->markAsRead();
+    }
+
+    // 调用修改器，方法名固定命名方法
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于60，即认为是已经做过加密的情况
+        if(strlen($value) != 60) {
+            // 不等于60，做加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是`http`子串开头，那就是从后台上传的，需要补全URL
+        if (! Str::startsWith($path, 'http')) {
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
     }
 
 }
