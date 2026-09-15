eb4cf3689224780f5b25bed00f7d369ce63dc4c7
MrZhang <zslemail@qq.com>
2026-09-15 16:29:11 +0800

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,9 +9,12 @@ use App\Models\User;
 
 class UserObserver
 {
-    public function creating(User $user)
+    public function saving(User $user)
     {
-        //
+        //这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'http://larabbs.test:30091/uploads/images/avatars/202609/15/11_1789460600_WAqJ34eb7K.png';
+        }
     }
 
     public function updating(User $user)
